f45ef774c0535e59835c45c575db99eb1b753290
frangu617 <142462584+frangu617@users.noreply.github.com>
2024-03-18 21:42:15 -0700

    assignment done

--- petfax/facts.py
+++ petfax/facts.py
@@ -10,10 +10,15 @@ bp = Blueprint('fact', __name__, url_prefix='/facts')
 @bp.get('/')
 def index():
     facts = mongo.db.facts.find()
-    return render_template('facts.html', facts=facts)
+    return render_template('facts_index.html')
 
-@bp.route('/new', methods=['POST'])
+@bp.route('/new', methods=['GET', 'POST'])
 def new():
-    fact_text = request.form['fact']
-    mongo.db.facts.insert_one({'fact_text': fact_text})
+    if request.method == 'POST':
+        fact_text = request.form['fact']
+        mongo.db.facts.insert_one({'fact_text': fact_text})
+        # Redirect to prevent form resubmission on refresh
+        return redirect(url_for('fact.index'))
+
+    # For a GET request, just render the template with the form
     return render_template('facts.html')
